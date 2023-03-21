901db666d9be86dd2dbc8c28e8ece54d9fe160b7
Asato65 <a.3ka319@gmail.com>
2023-03-21 22:55:25 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 "use strict";
 
 const clog = console.log;
+const fileList = {};
 
 let nesData, dbgData;
 let nesDv;
 
 $(function() {
+	["smb1.asm"]
+	.forEach(el => {
+		$.ajax({
+			type: "POST",
+			url: `./asmFile/${el}`,
+			data: "",
+			dataType: "text"
+		}).done(data => {
+			fileList[el] = data;
+		}).fail(data => {
+			alert(data);
+		});
+	})
+
+	$.ajax({
+		type: "POST",
+		url: "smb1.dbg",
+		data: "",
+		dataType: "text"
+	}).done(data => {
+		dbgData = data;
+	}).fail(data => {
+		alert(data);
+	});
+
 	$(document)
 	.on('click', '#load_data', e => {
 		let nesFiles = $('#nesfile')[0].files;
-		let dbgFiles = $('#dbgfile')[0].files;
 		let nesFile = nesFiles[0];
-		let dbgFile = dbgFiles[0];
-		/* develop
-		if (!nesFiles.length || !dbgFiles.length || !nesFile || !dbgFile) {
-			alert('Please select both nes and dbg file.')
+		if (!nesFiles.length || !nesFile) {
+			alert('Please select both nes file.')
 			return;
-		}*/
-		if (nesFile.size + dbgFile.size > 50 * 1024 * 1024) {  // 50 * 1000KB = 50MB
+		}
+		if (nesFile.size > 50 * 1024 * 1024) {  // 50 * 1000KB = 50MB
 			alert('File size over.');
 			return true;
 		}
@@ -30,25 +53,25 @@ $(function() {
 		.catch((err) => {
 			alert(err);
 		});
-
-		readText(dbgFile)
-		.then(data => {
-			dbgData = data;
-			// 値のセット方法： dbgDv.setUint8(1, 66);
-		})
-		.catch(err => {
-			alert(err);
-		});
 	})
 	.on('click', '.get_variable_val', e => {
-		let variable_name = $(e.target).closest('div').find('.variable_name').val();
-		let reg = new RegExp(`${variable_name} = .*$`);
-		clog('reg: ', `name="${variable_name}"`)
-		let res = dbgData.match(reg)//[0].slice(variable_name.length + 3);
-		clog(res);
+		let variableName = $(e.target).closest('div').find('.variable_name').val();
+		let reg = new RegExp(variableName, "g");
+
+		Object.keys(fileList).forEach(key => {
+			let posArr = [];
+			let reg2 = new RegExp(`id=(\\d+),name="${key}"`);
+			let fileNum = dbgData.match(reg2)[0].replace(reg2, '$1');
+			for(const el of fileList[key].matchAll(reg)) posArr.push(el.index);
+			posArr.forEach(pos => {
+				let tmp = fileList[key].slice(0, pos).match(/\r\n|\r|\n/g)
+				let line = tmp ? tmp.length : 0;
+				clog(`id=(\\d+),file=${fileNum},line=${line}`)
+				let reg3 = new RegExp(`id=(\\d+),file=${fileNum},line=${line}`)
+			});
+		});
 	})
 });
-
 
 
 function readBinary(file) {
@@ -66,23 +89,5 @@ function readBinary(file) {
 		}
 
 		reader.readAsArrayBuffer(file);
-	});
-}
-
-function readText(file) {
-	const reader = new FileReader();
-
-	return new Promise((resolve, reject) => {
-		reader.onerror = () => {
-			reader.abort();
-			reject('Text load error.');
-		}
-
-		reader.onload = () => {
-			clog('Load text file.');
-			resolve(reader.result);
-		}
-
-		reader.readAsText(file);
 	});
 }
